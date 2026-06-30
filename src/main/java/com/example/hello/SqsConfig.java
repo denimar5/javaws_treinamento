591c729ef9b59d4f8fe484a0965e3c23f4f8289b
Denilson Marcos <denilson.marcos@gmail.com>
2026-06-29 22:07:52 -0300

    sns

--- src/main/java/com/example/hello/SqsConfig.java
+++ src/main/java/com/example/hello/SqsConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
-public class S3Config {
+public class SqsConfig {
 
     @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
                 .region(Region.US_EAST_2)
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
